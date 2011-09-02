9a1f16d1e6f0d21c0e3c046a5d6df85db86fe342
Mohammad Islam <kamrul@apache.org>
2011-09-02 00:35:25 +0000

    Closes GH-0384 Adding new workflow jpa commands.
    Closes GH-0384 Fixing Java doc
    
    Closes GH-0384 Adding Test Cases
    
    Closes GH-0384 Modifying Test Case
    
    Closes GH-0384 Fixing Java Docs
    
    From: Mayank Bansal <bansalm@l2tp-8-241.corp.yahoo.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1168681 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/oozie/executor/jpa/WorkflowActionSubsetGetJPAExecutor.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.oozie.executor.jpa;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.oozie.ErrorCode;
+import org.apache.oozie.WorkflowActionBean;
+import org.apache.oozie.util.ParamChecker;
+import org.apache.openjpa.persistence.OpenJPAPersistence;
+
+/**
+ * JPA Command to get subset of workflow actions for a particular workflow.
+ */
+public class WorkflowActionSubsetGetJPAExecutor implements JPAExecutor<List<WorkflowActionBean>> {
+
+    private final String wfId;
+    private final int start;
+    private final int length;
+
+    /**
+     * This Constructor creates the WorkflowActionSubsetGetJPAExecutor object Which gets the List of wrokflow action
+     * bean.
+     * 
+     * @param wfId
+     * @param start
+     * @param length
+     */
+    public WorkflowActionSubsetGetJPAExecutor(String wfId, int start, int length) {
+        ParamChecker.notNull(wfId, "wfJobId");
+        this.wfId = wfId;
+        this.start = start;
+        this.length = length;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.executor.jpa.JPAExecutor#execute(javax.persistence.EntityManager)
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<WorkflowActionBean> execute(EntityManager em) throws JPAExecutorException {
+        List<WorkflowActionBean> actions;
+        List<WorkflowActionBean> actionList = new ArrayList<WorkflowActionBean>();
+        try {
+            Query q = em.createNamedQuery("GET_ACTIONS_FOR_WORKFLOW");
+            OpenJPAPersistence.cast(q);
+            q.setParameter("wfId", wfId);
+            q.setFirstResult(start - 1);
+            q.setMaxResults(length);
+            actions = q.getResultList();
+            for (WorkflowActionBean a : actions) {
+                WorkflowActionBean aa = getBeanForRunningAction(a);
+                actionList.add(aa);
+            }
+        }
+        catch (Exception e) {
+            throw new JPAExecutorException(ErrorCode.E0605, e);
+        }
+        return actionList;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.executor.jpa.JPAExecutor#getName()
+     */
+    @Override
+    public String getName() {
+        return "WorkflowActionSubsetGetJPAExecutor";
+    }
+
+    private WorkflowActionBean getBeanForRunningAction(WorkflowActionBean a) throws SQLException {
+        if (a != null) {
+            WorkflowActionBean action = new WorkflowActionBean();
+            action.setId(a.getId());
+            action.setConf(a.getConf());
+            action.setConsoleUrl(a.getConsoleUrl());
+            action.setData(a.getData());
+            action.setErrorInfo(a.getErrorCode(), a.getErrorMessage());
+            action.setExternalId(a.getExternalId());
+            action.setExternalStatus(a.getExternalStatus());
+            action.setName(a.getName());
+            action.setRetries(a.getRetries());
+            action.setTrackerUri(a.getTrackerUri());
+            action.setTransition(a.getTransition());
+            action.setType(a.getType());
+            action.setEndTime(a.getEndTime());
+            action.setExecutionPath(a.getExecutionPath());
+            action.setLastCheckTime(a.getLastCheckTime());
+            action.setLogToken(a.getLogToken());
+            if (a.getPending() == true) {
+                action.setPending();
+            }
+            action.setPendingAge(a.getPendingAge());
+            action.setSignalValue(a.getSignalValue());
+            action.setSlaXml(a.getSlaXml());
+            action.setStartTime(a.getStartTime());
+            action.setStatus(a.getStatus());
+            action.setJobId(a.getWfId());
+            return action;
+        }
+        return null;
+    }
+}
---
+++ core/src/main/java/org/apache/oozie/executor/jpa/WorkflowsJobGetJPAExecutor.java
@@ -0,0 +1,298 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.oozie.executor.jpa;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.oozie.WorkflowJobBean;
+import org.apache.oozie.WorkflowsInfo;
+import org.apache.oozie.client.OozieClient;
+import org.apache.oozie.client.WorkflowJob.Status;
+import org.apache.oozie.util.XLog;
+import org.apache.openjpa.persistence.OpenJPAPersistence;
+import org.apache.openjpa.persistence.OpenJPAQuery;
+import org.apache.openjpa.persistence.jdbc.FetchDirection;
+import org.apache.openjpa.persistence.jdbc.JDBCFetchPlan;
+import org.apache.openjpa.persistence.jdbc.LRSSizeAlgorithm;
+import org.apache.openjpa.persistence.jdbc.ResultSetType;
+
+public class WorkflowsJobGetJPAExecutor implements JPAExecutor<WorkflowsInfo> {
+
+    private static final String seletStr = "Select w.id, w.appName, w.status, w.run, w.user, w.group, w.createdTimestamp, "
+        + "w.startTimestamp, w.lastModifiedTimestamp, w.endTimestamp from WorkflowJobBean w";
+    private static final String countStr = "Select count(w) from WorkflowJobBean w";
+
+    private final Map<String, List<String>> filter;
+    private final int start;
+    private final int len;
+
+    /**
+     * This JPA Executor gets the workflows info for the range.
+     * 
+     * @param filter
+     * @param start
+     * @param len
+     */
+    public WorkflowsJobGetJPAExecutor(Map<String, List<String>> filter, int start, int len) {
+        this.filter = filter;
+        this.start = start;
+        this.len = len;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.executor.jpa.JPAExecutor#execute(javax.persistence.EntityManager)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public WorkflowsInfo execute(EntityManager em) throws JPAExecutorException {
+        List<String> orArray = new ArrayList<String>();
+        List<String> colArray = new ArrayList<String>();
+        List<String> valArray = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder("");
+        boolean isStatus = false;
+        boolean isGroup = false;
+        boolean isAppName = false;
+        boolean isUser = false;
+        boolean isEnabled = false;
+        int index = 0;
+        for (Map.Entry<String, List<String>> entry : filter.entrySet()) {
+            String colName = null;
+            String colVar = null;
+            if (entry.getKey().equals(OozieClient.FILTER_GROUP)) {
+                List<String> values = filter.get(OozieClient.FILTER_GROUP);
+                colName = "group";
+                for (int i = 0; i < values.size(); i++) {
+                    colVar = "group";
+                    colVar = colVar + index;
+                    if (!isEnabled && !isGroup) {
+                        sb.append(seletStr).append(" where w.group IN (:group" + index);
+                        isGroup = true;
+                        isEnabled = true;
+                    }
+                    else {
+                        if (isEnabled && !isGroup) {
+                            sb.append(" and w.group IN (:group" + index);
+                            isGroup = true;
+                        }
+                        else {
+                            if (isGroup) {
+                                sb.append(", :group" + index);
+                            }
+                        }
+                    }
+                    if (i == values.size() - 1) {
+                        sb.append(")");
+                    }
+                    index++;
+                    valArray.add(values.get(i));
+                    orArray.add(colName);
+                    colArray.add(colVar);
+                }
+            }
+            else {
+                if (entry.getKey().equals(OozieClient.FILTER_STATUS)) {
+                    List<String> values = filter.get(OozieClient.FILTER_STATUS);
+                    colName = "status";
+                    for (int i = 0; i < values.size(); i++) {
+                        colVar = "status";
+                        colVar = colVar + index;
+                        if (!isEnabled && !isStatus) {
+                            sb.append(seletStr).append(" where w.status IN (:status" + index);
+                            isStatus = true;
+                            isEnabled = true;
+                        }
+                        else {
+                            if (isEnabled && !isStatus) {
+                                sb.append(" and w.status IN (:status" + index);
+                                isStatus = true;
+                            }
+                            else {
+                                if (isStatus) {
+                                    sb.append(", :status" + index);
+                                }
+                            }
+                        }
+                        if (i == values.size() - 1) {
+                            sb.append(")");
+                        }
+                        index++;
+                        valArray.add(values.get(i));
+                        orArray.add(colName);
+                        colArray.add(colVar);
+                    }
+                }
+                else {
+                    if (entry.getKey().equals(OozieClient.FILTER_NAME)) {
+                        List<String> values = filter.get(OozieClient.FILTER_NAME);
+                        colName = "appName";
+                        for (int i = 0; i < values.size(); i++) {
+                            colVar = "appName";
+                            colVar = colVar + index;
+                            if (!isEnabled && !isAppName) {
+                                sb.append(seletStr).append(" where w.appName IN (:appName" + index);
+                                isAppName = true;
+                                isEnabled = true;
+                            }
+                            else {
+                                if (isEnabled && !isAppName) {
+                                    sb.append(" and w.appName IN (:appName" + index);
+                                    isAppName = true;
+                                }
+                                else {
+                                    if (isAppName) {
+                                        sb.append(", :appName" + index);
+                                    }
+                                }
+                            }
+                            if (i == values.size() - 1) {
+                                sb.append(")");
+                            }
+                            index++;
+                            valArray.add(values.get(i));
+                            orArray.add(colName);
+                            colArray.add(colVar);
+                        }
+                    }
+                    else {
+                        if (entry.getKey().equals(OozieClient.FILTER_USER)) {
+                            List<String> values = filter.get(OozieClient.FILTER_USER);
+                            colName = "user";
+                            for (int i = 0; i < values.size(); i++) {
+                                colVar = "user";
+                                colVar = colVar + index;
+                                if (!isEnabled && !isUser) {
+                                    sb.append(seletStr).append(" where w.user IN (:user" + index);
+                                    isUser = true;
+                                    isEnabled = true;
+                                }
+                                else {
+                                    if (isEnabled && !isUser) {
+                                        sb.append(" and w.user IN (:user" + index);
+                                        isUser = true;
+                                    }
+                                    else {
+                                        if (isUser) {
+                                            sb.append(", :user" + index);
+                                        }
+                                    }
+                                }
+                                if (i == values.size() - 1) {
+                                    sb.append(")");
+                                }
+                                index++;
+                                valArray.add(values.get(i));
+                                orArray.add(colName);
+                                colArray.add(colVar);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int realLen = 0;
+
+        Query q = null;
+        Query qTotal = null;
+        if (orArray.size() == 0) {
+            q = em.createNamedQuery("GET_WORKFLOWS_COLUMNS");
+            q.setFirstResult(start - 1);
+            q.setMaxResults(len);
+            qTotal = em.createNamedQuery("GET_WORKFLOWS_COUNT");
+        }
+        else {
+            if (orArray.size() > 0) {
+                StringBuilder sbTotal = new StringBuilder(sb);
+                sb.append(" order by w.startTimestamp desc ");
+                XLog.getLog(getClass()).debug("Created String is **** " + sb.toString());
+                q = em.createQuery(sb.toString());
+                q.setFirstResult(start - 1);
+                q.setMaxResults(len);
+                qTotal = em.createQuery(sbTotal.toString().replace(seletStr, countStr));
+                for (int i = 0; i < orArray.size(); i++) {
+                    q.setParameter(colArray.get(i), valArray.get(i));
+                    qTotal.setParameter(colArray.get(i), valArray.get(i));
+                }
+            }
+        }
+
+        OpenJPAQuery kq = OpenJPAPersistence.cast(q);
+        JDBCFetchPlan fetch = (JDBCFetchPlan) kq.getFetchPlan();
+        fetch.setFetchBatchSize(20);
+        fetch.setResultSetType(ResultSetType.SCROLL_INSENSITIVE);
+        fetch.setFetchDirection(FetchDirection.FORWARD);
+        fetch.setLRSSizeAlgorithm(LRSSizeAlgorithm.LAST);
+        List<?> resultList = q.getResultList();
+        List<Object[]> objectArrList = (List<Object[]>) resultList;
+        List<WorkflowJobBean> wfBeansList = new ArrayList<WorkflowJobBean>();
+
+        for (Object[] arr : objectArrList) {
+            WorkflowJobBean ww = getBeanForWorkflowFromArray(arr);
+            wfBeansList.add(ww);
+        }
+
+        realLen = ((Long) qTotal.getSingleResult()).intValue();
+
+        return new WorkflowsInfo(wfBeansList, start, len, realLen);
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.oozie.executor.jpa.JPAExecutor#getName()
+     */
+    @Override
+    public String getName() {
+        return "WorkflowsJobGetJPAExecutor";
+    }
+
+    private WorkflowJobBean getBeanForWorkflowFromArray(Object[] arr) {
+
+        WorkflowJobBean wfBean = new WorkflowJobBean();
+        wfBean.setId((String) arr[0]);
+        if (arr[1] != null) {
+            wfBean.setAppName((String) arr[1]);
+        }
+        if (arr[2] != null) {
+            wfBean.setStatus(Status.valueOf((String) arr[2]));
+        }
+        if (arr[3] != null) {
+            wfBean.setRun((Integer) arr[3]);
+        }
+        if (arr[4] != null) {
+            wfBean.setUser((String) arr[4]);
+        }
+        if (arr[5] != null) {
+            wfBean.setGroup((String) arr[5]);
+        }
+        if (arr[6] != null) {
+            wfBean.setCreatedTime((Timestamp) arr[6]);
+        }
+        if (arr[7] != null) {
+            wfBean.setStartTime((Timestamp) arr[7]);
+        }
+        if (arr[8] != null) {
+            wfBean.setLastModifiedTime((Timestamp) arr[8]);
+        }
+        if (arr[9] != null) {
+            wfBean.setEndTime((Timestamp) arr[9]);
+        }
+        return wfBean;
+    }
+}
