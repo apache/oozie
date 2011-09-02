320fae8c2bfaddf8b5859fd42b3b6b70f7d14fc1
Mohammad Islam <kamrul@apache.org>
2011-09-02 00:36:21 +0000

    Closes GH-0075 Provide access to date list.
    From: Mohammad Kamrul Islam <kamrul@theybeside-lm.(none)>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1168714 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/main/java/org/apache/oozie/example/TimeUnit.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.oozie.example;
+
+import java.util.Calendar;
+
+public enum TimeUnit {
+	MINUTES(Calendar.MINUTE), HOURS(Calendar.HOUR), DAYS(Calendar.DATE), MONTHS(
+			Calendar.MONTH);
+
+	private int calendarUnit;
+
+	private TimeUnit(int calendarUnit) {
+		this.calendarUnit = calendarUnit;
+	}
+
+	/**
+	 * @return calendar unit
+	 */
+	public int getCalendarUnit() {
+		return calendarUnit;
+	}
+
+}
