0827f4dcbd203d10e0e4e6aee25fd9a8847c6170
Mohammad Islam <kamrul@apache.org>
2011-09-02 00:37:25 +0000

    Closes GH-0578 Fix runHudsonCIBuild.sh for tmp maven repo
    From: Angelo Kaichen Huang <angelokh@ymail.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1168757 13f79535-47bb-0310-9956-ffa450edef68

--- builds/runHudsonCIBuild.sh
+++ builds/runHudsonCIBuild.sh
@@ -9,12 +9,15 @@
 # You can also explicitly change JAVA_HOME or M3_HOME below.
 
 export JAVA_HOME=${TOOLS_HOME}/java/latest
-export M3_HOME=${TOOLS_HOME}/maven/apache-maven-3.0.2
+export M3_HOME=${TOOLS_HOME}/maven/apache-maven-3.0.3
 export PATH=$JAVA_HOME/bin:$M3_HOME/bin:$PATH
 
-#Please uncomment this line if the git repo needs to download
+#Please uncomment these lines if the git repo needs to download
 #git clone -o origin $OOZIE_GIT_REPO $WORKSPACE
+#cd $WORKSPACE
 
-rm -fr ~/.m2
-
-bin/mkdistro.sh $1
+M2DIR=`mktemp -d /tmp/oozie-m2.XXXXX`
+bin/mkdistro.sh $1 -Dmaven.repo.local=$M2DIR
+EXIT=$?
+rm -rf $M2DIR
+exit $EXIT
