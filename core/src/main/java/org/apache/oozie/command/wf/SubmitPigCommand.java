61b483da2e99b30b1181d2436de33756b9e4c0f7
Mohammad Islam <kamrul@apache.org>
2011-09-02 00:34:34 +0000

    Closes GH-0238 it should be possible to submit oozie pig jobs using the system library by default
    Removing unnecessary imports
    
    From: Alejandro Abdelnur <tucu00@gmail.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1168644 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/command/wf/SubmitPigCommand.java
+++ core/src/main/java/org/apache/oozie/command/wf/SubmitPigCommand.java
@@ -15,8 +15,6 @@
 package org.apache.oozie.command.wf;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.oozie.client.OozieClient;
-import org.apache.oozie.service.Services;
 import org.apache.oozie.util.XmlUtils;
 import org.jdom.Element;
 import org.jdom.Namespace;
