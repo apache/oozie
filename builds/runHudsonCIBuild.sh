8c9d375d3c0e1e43123a106c69b7ab5e67879718
Mohammad Islam <kamrul@apache.org>
2011-09-02 00:35:53 +0000

    Closes GH-0442 Add Hudson CI script to master branch
    From: Angelo Kaichen Huang <angelokh@ymail.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1168696 13f79535-47bb-0310-9956-ffa450edef68

---
+++ builds/runHudsonCIBuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# To run build in hudson, the default env variables should be defined in the environment. If not, please do the exports before
+# invoking this script.
+# export WORKSPACE=PATH_TO_WORKSPACE
+# export TOOLS_HOME=PATH_TO_COMMON_LIBRAARY_HOME
+# export OOZIE_GIT_REPO=git://github.com/yahoo/oozie.git
+
+# You can also explicitly change JAVA_HOME or M3_HOME below.
+
+export JAVA_HOME=${TOOLS_HOME}/java/latest
+export M3_HOME=${TOOLS_HOME}/maven/apache-maven-3.0.2
+export PATH=$JAVA_HOME/bin:$M3_HOME/bin:$PATH
+
+echo "WORKSPACE : $WORKSPACE" 
+echo "PATH : $PATH"
+
+#Please uncomment this line if the git repo needs to download
+#git clone -o origin $OOZIE_GIT_REPO $WORKSPACE
+
+rm ~/.m2
+
+cd $WORKSPACE;
+bin/mkdistro.sh
