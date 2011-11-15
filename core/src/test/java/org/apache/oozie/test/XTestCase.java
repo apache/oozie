e7ef9b8de75287c98bc1e0738b0c9115e10b64a6
Alejandro Abdelnur <tucu@apache.org>
2011-11-15 22:47:53 +0000

    OOZIE-602 Update the Hadoop version to be an Apache Hadoop version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1202452 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/oozie/test/XTestCase.java
+++ core/src/test/java/org/apache/oozie/test/XTestCase.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,6 +41,7 @@ import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MiniMRCluster;
+import org.apache.hadoop.security.authorize.ProxyUsers;
 import org.apache.oozie.BundleActionBean;
 import org.apache.oozie.BundleJobBean;
 import org.apache.oozie.CoordinatorActionBean;
@@ -687,6 +688,7 @@ public abstract class XTestCase extends TestCase {
             JobConf jobConf = mrCluster.createJobConf();
             System.setProperty(OOZIE_TEST_JOB_TRACKER, jobConf.get("mapred.job.tracker"));
             System.setProperty(OOZIE_TEST_NAME_NODE, jobConf.get("fs.default.name"));
+            ProxyUsers.refreshSuperUserGroupsConfiguration(conf);
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 @Override
                 public void run() {
