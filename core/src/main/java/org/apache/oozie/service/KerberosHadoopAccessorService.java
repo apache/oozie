e8fc9c64e3f1b0a3e510353888006fcdd60e4dcc
Alejandro Abdelnur <tucu@apache.org>
2011-11-22 20:54:52 +0000

    OOZIE-565 Make Oozie compile against Hadoop 0.23. (tucu)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1205165 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/service/KerberosHadoopAccessorService.java
+++ core/src/main/java/org/apache/oozie/service/KerberosHadoopAccessorService.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,10 +31,6 @@ import org.apache.oozie.util.XLog;
 import org.apache.oozie.util.XConfiguration;
 import org.apache.oozie.util.ParamChecker;
 import org.apache.oozie.ErrorCode;
-import org.apache.oozie.service.HadoopAccessorService;
-import org.apache.oozie.service.HadoopAccessorException;
-import org.apache.oozie.service.Service;
-import org.apache.oozie.service.ServiceException;
 
 import java.io.IOException;
 import java.net.URI;
@@ -129,6 +125,7 @@ public class KerberosHadoopAccessorService extends HadoopAccessorService {
             UserGroupInformation ugi = getUGI(user);
             JobClient jobClient = ugi.doAs(new PrivilegedExceptionAction<JobClient>() {
                 public JobClient run() throws Exception {
+                    conf.set("mapreduce.framework.name", "yarn");
                     return new JobClient(conf);
                 }
             });
