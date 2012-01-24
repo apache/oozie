a036914e0e11a617511ce7e48e676f6b8fab7eaf
Mohammad Islam <kamrul@apache.org>
2012-01-24 07:34:16 +0000

    OOZIE-642: Year support in dateOffset() El function.(Virag via Mohammad)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1235153 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/oozie/coord/TestCoordELFunctions.java
+++ core/src/test/java/org/apache/oozie/coord/TestCoordELFunctions.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -359,6 +359,9 @@ public class TestCoordELFunctions extends XTestCase {
 
         expr = "${coord:dateOffset(\"2009-09-08T23:59Z\", -1, \"DAY\")}";
         assertEquals("2009-09-07T23:59Z", CoordELFunctions.evalAndWrap(eval, expr));
+
+        expr = "${coord:dateOffset(\"2009-09-08T23:59Z\", 1, \"YEAR\")}";
+        assertEquals("2010-09-08T23:59Z", CoordELFunctions.evalAndWrap(eval, expr));
     }
 
     public void testCurrent() throws Exception {
