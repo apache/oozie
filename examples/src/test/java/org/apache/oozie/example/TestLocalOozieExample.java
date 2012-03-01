84d79b75b3937d67ac492b5b3434cf7e388abe3a
Alejandro Abdelnur <tucu@apache.org>
2012-03-01 18:46:57 +0000

    OOZIE-715 Fix TestLocalOozieExample (angeloh via tucu)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1295731 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/oozie/example/TestLocalOozieExample.java
+++ examples/src/test/java/org/apache/oozie/example/TestLocalOozieExample.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,26 +21,30 @@ import junit.framework.TestCase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.oozie.DagEngine;
 import org.apache.oozie.service.Services;
 import org.apache.oozie.service.WorkflowAppService;
+import org.apache.oozie.test.XFsTestCase;
 import org.apache.oozie.test.XTestCase;
 import org.apache.oozie.util.IOUtils;
 import org.apache.oozie.util.ParamChecker;
 import org.apache.oozie.util.XLog;
 import org.apache.oozie.util.XConfiguration;
 import org.apache.oozie.action.hadoop.DoAs;
+import org.apache.oozie.client.OozieClient;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
-public class TestLocalOozieExample extends XTestCase {
+public class TestLocalOozieExample extends XFsTestCase {
     private String oozieLocalLog;
     private String testDir;
     private FileSystem fileSystem;
     private Path fsTestDir;
 
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         File dir = new File(System.getProperty("oozie.test.dir", "/tmp"));
@@ -49,47 +53,8 @@ public class TestLocalOozieExample extends XTestCase {
         dir = new File(dir, getName());
         dir.mkdirs();
         testDir = dir.getAbsolutePath();
-        final Configuration conf = new Configuration();
-        conf.set(WorkflowAppService.HADOOP_USER, getTestUser());
-        conf.set("mapred.job.tracker", getJobTrackerUri());
-        conf.set("fs.default.name", getNameNodeUri());
-        injectKerberosInfo(conf);
 
-        // TODO restore this when getting rid of DoAs trick
-
-//        if (System.getProperty("oozie.test.kerberos", "off").equals("on")) {
-//            Configuration c = new Configuration();
-//            c.set("hadoop.security.authentication", "kerberos");
-//            UserGroupInformation.setConfiguration(c);
-//            String principal = System.getProperty("oozie.test.kerberos.oozie.principal",
-//                                                  System.getProperty("user.name") + "/localhost") + "@"
-//                    + System.getProperty("oozie.test.kerberos.realm", "LOCALHOST");
-//            String defaultFile = new File(System.getProperty("user.home"), "oozie.keytab").getAbsolutePath();
-//            String keytabFile = System.getProperty("oozie.test.kerberos.keytab.file", defaultFile);
-//            UserGroupInformation.loginUserFromKeytab(principal, keytabFile);
-////            System.setProperty("oozie.service.HadoopAccessorService.kerberos.enabled", "kerberos");
-//        }
-
-        Class klass;
-        try {
-            klass = Class.forName("org.apache.oozie.action.hadoop.KerberosDoAs");
-        }
-        catch (ClassNotFoundException ex) {
-            klass = DoAs.class;
-        }
-        DoAs doAs = (DoAs) klass.newInstance();
-        final FileSystem[] fs = new FileSystem[1];
-        doAs.setCallable(new Callable<Void>() {
-            public Void call() throws Exception {
-                Configuration defaultConf = new Configuration();
-                XConfiguration.copy(conf, defaultConf);
-                fs[0] = FileSystem.get(defaultConf);
-                return null;
-            }
-        });
-        doAs.setUser(getTestUser());
-        doAs.call();
-        fileSystem = fs[0];
+        fileSystem = getFileSystem();
 
         Path path = new Path(fileSystem.getWorkingDirectory(), "oozietests/" + getClass().getName() + "/" + getName());
         fsTestDir = fileSystem.makeQualified(path);
@@ -115,6 +80,7 @@ public class TestLocalOozieExample extends XTestCase {
         System.setProperty(Services.OOZIE_HOME_DIR, testCaseDir);
     }
 
+    @Override
     protected void delete(File file) throws IOException {
         ParamChecker.notNull(file, "file");
         if (file.getAbsolutePath().length() < 5) {
@@ -144,6 +110,7 @@ public class TestLocalOozieExample extends XTestCase {
         return dir.getAbsolutePath();
     }
 
+    @Override
     protected void tearDown() throws Exception {
         fileSystem = null;
         fsTestDir = null;
