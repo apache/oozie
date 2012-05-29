491e228b5d3e7259f1a50ae207f00aec1f84e436
Alejandro Abdelnur <tucu@apache.org>
2012-05-29 21:39:03 +0000

    Merge -r 1344003:1344004 from trunk to branch. FIXES: OOZIE-852
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/branches/branch-3.2@1344009 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/oozie/action/hadoop/TestPipesMain.java
+++ core/src/test/java/org/apache/oozie/action/hadoop/TestPipesMain.java
@@ -20,7 +20,6 @@ package org.apache.oozie.action.hadoop;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.filecache.DistributedCache;
-import org.apache.oozie.test.XTestCase;
 import org.apache.oozie.util.XConfiguration;
 import org.apache.oozie.util.IOUtils;
 
@@ -36,88 +35,74 @@ import java.net.URI;
 
 public class TestPipesMain extends MainTestCase {
 
-    public static String getProgramName(XTestCase tc) {
-        String osSuffix = null;
-
-        if (System.getProperty("os.name").startsWith("Mac") && System.getProperty("os.arch").equals("x86_64")) {
-            osSuffix = "Mac_OS_X-x86_64-64";
-        }
-        if (System.getProperty("os.name").startsWith("Linux") && System.getProperty("os.arch").equals("amd64")) {
-            osSuffix = "Linux-amd64-64";
-        }
-        if (System.getProperty("os.name").startsWith("Linux") && System.getProperty("os.arch").equals("i386")) {
-            osSuffix = "Linux-i386-32";
-        }
-        if (osSuffix == null) {
-            fail();
-        }
-
-        return "wordcount-simple" + "_" + osSuffix + "_h20";
-    }
-
     public Void call() throws Exception {
 
-        String wordCountBinary = getProgramName(this);
         Path programPath = new Path(getFsTestCaseDir(), "wordcount-simple");
 
         FileSystem fs = getFileSystem();
 
-        InputStream is = IOUtils.getResourceAsStream(wordCountBinary, -1);
-        OutputStream os = fs.create(programPath);
-        IOUtils.copyStream(is, os);
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("wordcount-simple");
+        if (is != null) {
+            OutputStream os = fs.create(programPath);
+            IOUtils.copyStream(is, os);
 
-        Path inputDir = new Path(getFsTestCaseDir(), "input");
-        fs.mkdirs(inputDir);
-        Writer writer = new OutputStreamWriter(fs.create(new Path(inputDir, "data.txt")));
-        writer.write("hello");
-        writer.close();
+            Path inputDir = new Path(getFsTestCaseDir(), "input");
+            fs.mkdirs(inputDir);
+            Writer writer = new OutputStreamWriter(fs.create(new Path(inputDir, "data.txt")));
+            writer.write("hello");
+            writer.close();
 
-        Path outputDir = new Path(getFsTestCaseDir(), "output");
+            Path outputDir = new Path(getFsTestCaseDir(), "output");
 
 
-        XConfiguration jobConf = new XConfiguration();
+            XConfiguration jobConf = new XConfiguration();
 
-        jobConf.set("user.name", getTestUser());
+            jobConf.set("user.name", getTestUser());
 
-        jobConf.setInt("mapred.map.tasks", 1);
-        jobConf.setInt("mapred.map.max.attempts", 1);
-        jobConf.setInt("mapred.reduce.max.attempts", 1);
+            jobConf.setInt("mapred.map.tasks", 1);
+            jobConf.setInt("mapred.map.max.attempts", 1);
+            jobConf.setInt("mapred.reduce.max.attempts", 1);
 
-        jobConf.set("mapred.job.tracker", getJobTrackerUri());
-        jobConf.set("fs.default.name", getNameNodeUri());
-        jobConf.set("mapreduce.framework.name", "yarn");
+            jobConf.set("mapred.job.tracker", getJobTrackerUri());
+            jobConf.set("fs.default.name", getNameNodeUri());
+            jobConf.set("mapreduce.framework.name", "yarn");
 
-        jobConf.set("mapred.input.dir", inputDir.toString());
-        jobConf.set("mapred.output.dir", outputDir.toString());
+            jobConf.set("mapred.input.dir", inputDir.toString());
+            jobConf.set("mapred.output.dir", outputDir.toString());
 
-        jobConf.set("oozie.pipes.program", programPath.toUri().getPath());
-        jobConf.setBoolean("hadoop.pipes.java.recordreader", true);
+            jobConf.set("oozie.pipes.program", programPath.toUri().getPath());
+            jobConf.setBoolean("hadoop.pipes.java.recordreader", true);
 
-        
-        DistributedCache.addCacheFile(new URI(programPath.toUri().getPath()), fs.getConf());
 
-        File actionXml = new File(getTestCaseDir(), "action.xml");
-        os = new FileOutputStream(actionXml);
-        jobConf.writeXml(os);
-        os.close();
+            DistributedCache.addCacheFile(new URI(programPath.toUri().getPath()), fs.getConf());
 
-        File newIdProperties = new File(getTestCaseDir(), "newId.properties");
+            File actionXml = new File(getTestCaseDir(), "action.xml");
+            os = new FileOutputStream(actionXml);
+            jobConf.writeXml(os);
+            os.close();
 
-        System.setProperty("oozie.action.conf.xml", actionXml.getAbsolutePath());
-        System.setProperty("oozie.action.newId.properties", newIdProperties.getAbsolutePath());
+            File newIdProperties = new File(getTestCaseDir(), "newId.properties");
 
-        String[] args = {};
+            System.setProperty("oozie.action.conf.xml", actionXml.getAbsolutePath());
+            System.setProperty("oozie.action.newId.properties", newIdProperties.getAbsolutePath());
 
-        PipesMain.main(args);
+            String[] args = {};
 
-        assertTrue(newIdProperties.exists());
+            PipesMain.main(args);
 
-        is = new FileInputStream(newIdProperties);
-        Properties props = new Properties();
-        props.load(is);
-        is.close();
+            assertTrue(newIdProperties.exists());
 
-        assertTrue(props.containsKey("id"));
+            is = new FileInputStream(newIdProperties);
+            Properties props = new Properties();
+            props.load(is);
+            is.close();
+
+            assertTrue(props.containsKey("id"));
+        }
+        else {
+            System.out.println(
+                "SKIPPING TEST: TestPipesMain, binary 'wordcount-simple' not available in the classpath");
+        }
         return null;
     }
 
