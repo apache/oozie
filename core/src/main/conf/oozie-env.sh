99398d4ea1ae95b91bbd516f60451de14e6c3fb7
Alejandro Abdelnur <tucu@apache.org>
2012-07-25 21:44:57 +0000

    OOZIE-924 CATALINA_OPTS should include  in oozie-env.sh (rkanter via tucu)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/oozie/trunk@1365793 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/conf/oozie-env.sh
+++ core/src/main/conf/oozie-env.sh
@@ -22,7 +22,7 @@
 # Settings for the Embedded Tomcat that runs Oozie
 # Java System properties for Oozie should be specified in this variable
 #
-export CATALINA_OPTS=-Xmx1024m
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx1024m"
 
 # Oozie configuration file to load from Oozie configuration directory
 #
