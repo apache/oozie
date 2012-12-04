83da0c650a8eee83d6109598506980c439567aca
Virag Kothari <virag@apache.org>
2012-12-04 20:32:50 +0000

    OOZIE-1073 Optimize latest and future EL resolution in case of start-instance and end-instance (rohini via virag)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1417158 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/command/coord/CoordCommandUtils.java
+++ core/src/main/java/org/apache/oozie/command/coord/CoordCommandUtils.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -231,16 +231,13 @@ public class CoordCommandUtils {
                     }
                 }
                 else { // latest(n)/future() EL is present
-                    for (; startIndex <= endIndex; startIndex++) {
-                        if (instances.length() > 0) {
-                            instances.append(CoordELFunctions.INSTANCE_SEPARATOR);
-                        }
-                        if (funcType == LATEST) {
-                            instances.append("${coord:latest(").append(startIndex).append(")}");
-                        }
-                        else if (funcType == FUTURE) {
-                            instances.append("${coord:future(").append(startIndex).append(",'").append(endRestArg).append("')}");
-                        }
+                    if (funcType == LATEST) {
+                        instances.append("${coord:latestRange(").append(startIndex).append(",").append(endIndex)
+                                .append(")}");
+                    }
+                    else if (funcType == FUTURE) {
+                        instances.append("${coord:futureRange(").append(startIndex).append(",").append(endIndex)
+                                .append(",'").append(endRestArg).append("')}");
                     }
                 }
             }
