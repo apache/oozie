84ff4b97be4ff6869d42ec8aeeee86e52594b312
Virag Kothari <virag@apache.org>
2012-12-14 00:07:20 +0000

    OOZIE-1130 Upgrade from 3.2 to 3.3 failing due to change in WorkflowInstance structure (virag)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1421622 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/workflow/lite/JoinNodeDef.java
+++ core/src/main/java/org/apache/oozie/workflow/lite/JoinNodeDef.java
@@ -17,7 +17,12 @@
  */
 package org.apache.oozie.workflow.lite;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import org.apache.oozie.ErrorCode;
+import org.apache.oozie.workflow.WorkflowException;
 
 /**
  * Node definition for JOIN control node.
@@ -31,4 +36,61 @@ public class JoinNodeDef extends ControlNodeDef {
         super(name, "", klass, Arrays.asList(transition));
     }
 
+    public static class JoinNodeHandler extends NodeHandler {
+
+        public void loopDetection(Context context) throws WorkflowException {
+            String flag = getLoopFlag(context.getNodeDef().getName());
+            if (context.getVar(flag) != null) {
+                throw new WorkflowException(ErrorCode.E0709, context.getNodeDef().getName());
+            }
+            String parentExecutionPath = context.getParentExecutionPath(context.getExecutionPath());
+            String forkCount = context.getVar(ControlNodeHandler.FORK_COUNT_PREFIX + parentExecutionPath);
+            if (forkCount == null) {
+                throw new WorkflowException(ErrorCode.E0720, context.getNodeDef().getName());
+            }
+            int count = Integer.parseInt(forkCount) - 1;
+            if (count == 0) {
+                context.setVar(flag, "true");
+            }
+        }
+
+        public boolean enter(Context context) throws WorkflowException {
+            String parentExecutionPath = context.getParentExecutionPath(context.getExecutionPath());
+            String forkCount = context.getVar(ControlNodeHandler.FORK_COUNT_PREFIX + parentExecutionPath);
+            if (forkCount == null) {
+                throw new WorkflowException(ErrorCode.E0720, context.getNodeDef().getName());
+            }
+            int count = Integer.parseInt(forkCount) - 1;
+            if (count > 0) {
+                context.setVar(ControlNodeHandler.FORK_COUNT_PREFIX + parentExecutionPath, "" + count);
+                context.deleteExecutionPath();
+            }
+            else {
+                context.setVar(ControlNodeHandler.FORK_COUNT_PREFIX + parentExecutionPath, null);
+            }
+            return (count == 0);
+        }
+
+        public List<String> multiExit(Context context) {
+            String parentExecutionPath = context.getParentExecutionPath(context.getExecutionPath());
+            // NOW we delete..
+            context.deleteExecutionPath();
+
+            String transition = context.getNodeDef().getTransitions().get(0);
+            String fullTransition = context.createFullTransition(parentExecutionPath, transition);
+            List<String> transitions = new ArrayList<String>(1);
+            transitions.add(fullTransition);
+            return transitions;
+        }
+
+        public String exit(Context context) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void kill(Context context) {
+        }
+
+        public void fail(Context context) {
+        }
+    }
 }
