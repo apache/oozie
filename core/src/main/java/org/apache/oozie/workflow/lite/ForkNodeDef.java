9f104c98d67d888b9564a1a1b4f8d6db42426771
virag <virag@unknown>
2012-12-14 00:07:20 +0000

    OOZIE-1130 Upgrade from 3.2 to 3.3 failing due to change in WorkflowInstance structure (virag)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1421622 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/workflow/lite/ForkNodeDef.java
+++ core/src/main/java/org/apache/oozie/workflow/lite/ForkNodeDef.java
@@ -17,6 +17,7 @@
  */
 package org.apache.oozie.workflow.lite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +31,39 @@ public class ForkNodeDef extends ControlNodeDef {
     public ForkNodeDef(String name, Class<? extends ControlNodeHandler> klass,
                        List<String> transitions) {
         super(name, "", klass, transitions);
+    }
+
+    public static class ForkNodeHandler extends NodeHandler {
+
+        public boolean enter(Context context) {
+            return true;
+        }
+
+        // the return list contains (parentExecutionPath/transition#transition)+
+        public List<String> multiExit(Context context) {
+            List<String> transitions = context.getNodeDef().getTransitions();
+            context.setVar(ControlNodeHandler.FORK_COUNT_PREFIX + context.getExecutionPath(), "" + transitions.size());
+
+            List<String> fullTransitions = new ArrayList<String>(transitions.size());
+
+            for (String transition : transitions) {
+                String childExecutionPath = context.createExecutionPath(transition);
+                String fullTransition = context.createFullTransition(childExecutionPath, transition);
+                fullTransitions.add(fullTransition);
+            }
+            return fullTransitions;
+        }
+
+        public String exit(Context context) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void kill(Context context) {
+        }
+
+        public void fail(Context context) {
+        }
+
     }
 
 }
