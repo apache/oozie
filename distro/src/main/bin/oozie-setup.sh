c3f5a896c4f55f01ab9fa47232e5f3576d721a40
Alejandro Abdelnur <tucu@apache.org>
2013-04-18 22:38:25 +0000

    OOZIE-670 Merge addtowar and oozie-setup scripts into one (bowenzhangusa via tucu)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1469612 13f79535-47bb-0310-9956-ffa450edef68

--- distro/src/main/bin/oozie-setup.sh
+++ distro/src/main/bin/oozie-setup.sh
@@ -20,12 +20,8 @@
 function printUsage() {
   echo
   echo " Usage  : oozie-setup.sh <Command and OPTIONS>"
-  echo "          prepare-war [-hadoop HADOOP_VERSION HADOOP_PATH] [-extjs EXTJS_PATH] [-jars JARS_PATH] [-secure]"
-  echo "                      (prepare-war is to prepare war files for oozie)"
-  echo "                      (Hadoop version [0.20.1|0.20.2|0.20.104|0.20.200|0.23.x|2.x] and Hadoop install dir)"
-  echo "                      (EXTJS_PATH is expanded or ZIP, to enable the Oozie webconsole)"
-  echo "                      (JARS_PATH is multiple JAR path separated by ':')"
-  echo "                      (-secure will configure the war file to use HTTPS (SSL))"
+  echo "          prepare-war [-d directory] [-secure] (-d identifies an alternative directory for processing jars"
+  echo "                                                -secure will configure the war file to use HTTPS (SSL))"
   echo "          sharelib create -fs FS_URI [-locallib SHARED_LIBRARY] (create sharelib for oozie,"
   echo "                                                                FS_URI is the fs.default.name"
   echo "                                                                for hdfs uri; SHARED_LIBRARY, path to the"
@@ -37,11 +33,66 @@ function printUsage() {
   echo "                                                                in HDFS)"
   echo "          sharelib upgrade -fs FS_URI [-locallib SHARED_LIBRARY] (upgrade existing sharelib, fails if there"
   echo "                                                                  is no existing sharelib installed in HDFS)"
+  echo "          db create|upgrade|postupgrade -run [-sqlfile <FILE>] (create, upgrade or postupgrade oozie db with an"
+  echo "                                                                optional sql File)"
   echo "          (without options prints this usage information)"
   echo
   echo " EXTJS can be downloaded from http://www.extjs.com/learn/Ext_Version_Archives"
   echo
 }
+
+#Creating temporary directory
+function prepare() {
+  tmpDir=/tmp/oozie-war-packing-$$
+  rm -rf ${tmpDir}
+  mkdir ${tmpDir}
+  tmpWarDir=${tmpDir}/oozie-war
+  mkdir ${tmpWarDir}
+  checkExec "creating staging directory ${tmpDir}"
+}
+
+#cleans up temporary directory
+function cleanUp() {
+  if [ ! "${tmpDir}" = "" ]; then
+    rm -rf ${tmpDir}
+    checkExec "deleting staging directory ${tmpDir}"
+  fi
+}
+
+#check execution of command
+function checkExec() {
+  if [ $? -ne 0 ]
+  then
+    echo
+    echo "Failed: $1"
+    echo
+    cleanUp
+    exit -1;
+  fi
+}
+
+#check that a file/path exists
+function checkFileExists() {
+  if [ ! -e ${1} ]; then
+    echo
+    echo "File/Dir does no exist: ${1}"
+    echo
+    cleanUp
+    exit -1
+  fi
+}
+
+#check that a file/path does not exist
+function checkFileDoesNotExist() {
+  if [ -e ${1} ]; then
+    echo
+    echo "File/Dir already exists: ${1}"
+    echo
+    cleanUp
+    exit -1
+  fi
+}
+
 
 # resolve links - $0 may be a softlink
 PRG="${0}"
@@ -62,12 +113,10 @@ BASEDIR=`cd ${BASEDIR}/..;pwd`
 source ${BASEDIR}/bin/oozie-sys.sh -silent
 
 addExtjs=""
-addJars=""
 addHadoopJars=""
+additionalDir=""
 extjsHome=""
 jarsPath=""
-hadoopVersion=""
-hadoopPath=""
 prepareWar=""
 inputWar="${OOZIE_HOME}/oozie.war"
 outputWar="${CATALINA_BASE}/webapps/oozie.war"
@@ -77,13 +126,12 @@ secureConfigsDir="${CATALINA_BASE}/conf/ssl"
 
 while [ $# -gt 0 ]
 do
-  if [ "$1" = "sharelib" ]; then
-    shift
-    OOZIEFSCLI_OPTS="-Doozie.home.dir=${OOZIE_HOME}";
-    OOZIEFSCLI_OPTS="${OOZIEFSCLI_OPTS} -Doozie.config.dir=${OOZIE_CONFIG}";
-    OOZIEFSCLI_OPTS="${OOZIEFSCLI_OPTS} -Doozie.log.dir=${OOZIE_LOG}";
-    OOZIEFSCLI_OPTS="${OOZIEFSCLI_OPTS} -Doozie.data.dir=${OOZIE_DATA}";
-    OOZIEFSCLI_OPTS="${OOZIEFSCLI_OPTS} -Dderby.stream.error.file=${OOZIE_LOG}/derby.log"
+  if [ "$1" = "sharelib" ] || [ "$1" = "db" ]; then
+    OOZIE_OPTS="-Doozie.home.dir=${OOZIE_HOME}";
+    OOZIE_OPTS="${OOZIE_OPTS} -Doozie.config.dir=${OOZIE_CONFIG}";
+    OOZIE_OPTS="${OOZIE_OPTS} -Doozie.log.dir=${OOZIE_LOG}";
+    OOZIE_OPTS="${OOZIE_OPTS} -Doozie.data.dir=${OOZIE_DATA}";
+    OOZIE_OPTS="${OOZIE_OPTS} -Dderby.stream.error.file=${OOZIE_LOG}/derby.log"
 
     OOZIECPPATH=""
     OOZIECPPATH=${BASEDIR}/libtools/'*':${BASEDIR}/libext/'*'
@@ -94,52 +142,20 @@ do
       JAVA_BIN=${JAVA_HOME}/bin/java
     fi
 
-    ${JAVA_BIN} ${OOZIEFSCLI_OPTS} -cp ${OOZIECPPATH} org.apache.oozie.tools.OozieSharelibCLI "${@}"
+    if [ "$1" = "sharelib" ]; then
+      shift
+      ${JAVA_BIN} ${OOZIE_OPTS} -cp ${OOZIECPPATH} org.apache.oozie.tools.OozieSharelibCLI "${@}"
+    else
+      shift
+      ${JAVA_BIN} ${OOZIE_OPTS} -cp ${OOZIECPPATH} org.apache.oozie.tools.OozieDBCLI "${@}"
+    fi
     exit 0
-  elif [ "$1" = "-extjs" ]; then
-    shift
-    if [ $# -eq 0 ]; then
-      echo
-      echo "Missing option value, ExtJS path"
-      echo
-      printUsage
-      exit -1
-    fi
-    extjsHome=$1
-    addExtjs=true
-  elif [ "$1" = "-jars" ]; then
-    shift
-    if [ $# -eq 0 ]; then
-      echo
-      echo "Missing option value, JARs path"
-      echo
-      printUsage
-      exit -1
-    fi
-    jarsPath=$1
-    addJars=true
-  elif [ "$1" = "-hadoop" ]; then
-    shift
-    if [ $# -eq 0 ]; then
-      echo
-      echo "Missing option values, HADOOP_VERSION & HADOOP_HOME_PATH"
-      echo
-      printUsage
-      exit -1
-    elif [ $# -eq 1 ]; then
-      echo
-      echo "Missing option value, HADOOP_HOME_PATH"
-      echo
-      printUsage
-      exit -1
-    fi
-    hadoopVersion=$1
-    shift
-    hadoopPath=$1
-    addHadoopJars=true
   elif [ "$1" = "-secure" ]; then
     shift
     secure=true
+  elif [ "$1" = "-d" ]; then
+    shift
+    additionalDir=$1
   elif [ "$1" = "prepare-war" ]; then
     prepareWar=true
   else
@@ -158,7 +174,7 @@ fi
 
 echo
 
-if [ "${addExtjs}${addJars}${addHadoopJars}${prepareWar}" == "" ]; then
+if [ "${prepareWar}" == "" ]; then
   echo "no arguments given"
   printUsage
   exit -1
@@ -172,6 +188,10 @@ else
   # Adding extension JARs
 
   libext=${OOZIE_HOME}/libext
+  if [ "${additionalDir}" != "" ]; then
+    libext=${additionalDir}
+  fi
+
   if [ -d "${libext}" ]; then
     if [ `ls ${libext} | grep \.jar\$ | wc -c` != 0 ]; then
       for i in "${libext}/"*.jar; do
@@ -193,18 +213,30 @@ else
     fi
   fi
 
-  OPTIONS=""
-  if [ "${addExtjs}" != "" ]; then
-    OPTIONS="-extjs ${extjsHome}"
+  prepare
+
+  checkFileExists ${inputWar}
+  checkFileDoesNotExist ${outputWar}
+
+  if [ "${addExtjs}" = "true" ]; then
+    checkFileExists ${extjsHome}
   else
     echo "INFO: Oozie webconsole disabled, ExtJS library not specified"
   fi
-  if [ "${addJars}" != "" ]; then
-    OPTIONS="${OPTIONS} -jars ${jarsPath}"
+
+  if [ "${addJars}" = "true" ]; then
+      for jarPath in ${jarsPath//:/$'\n'}
+      do
+        checkFileExists ${jarPath}
+      done
   fi
-  if [ "${addHadoopJars}" != "" ]; then
-    OPTIONS="${OPTIONS} -hadoop ${hadoopVersion} ${hadoopPath}"
-  fi
+
+  #Unpacking original war
+  unzip ${inputWar} -d ${tmpWarDir} > /dev/null
+  checkExec "unzipping Oozie input WAR"
+
+  components=""
+
   if [ "${secure}" != "" ]; then
     OPTIONS="${OPTIONS} -secureWeb ${secureConfigsDir}/ssl-web.xml"
     #Use the SSL version of server.xml in oozie-server
@@ -215,7 +247,65 @@ else
     cp ${secureConfigsDir}/server.xml ${CATALINA_BASE}/conf/server.xml
   fi
 
-  ${OOZIE_HOME}/bin/addtowar.sh -inputwar ${inputWar} -outputwar ${outputWar} ${OPTIONS}
+  if [ "${addExtjs}" = "true" ]; then
+    if [ ! "${components}" = "" ];then
+      components="${components}, "
+    fi
+    components="${components}ExtJS library"
+    if [ -e ${tmpWarDir}/ext-2.2 ]; then
+      echo
+      echo "Specified Oozie WAR '${inputWar}' already contains ExtJS library files"
+      echo
+      cleanUp
+      exit -1
+    fi
+    #If the extjs path given is a ZIP, expand it and use it from there
+    if [ -f ${extjsHome} ]; then
+      unzip ${extjsHome} -d ${tmpDir} > /dev/null
+      extjsHome=${tmpDir}/ext-2.2
+    fi
+    #Inject the library in oozie war
+    cp -r ${extjsHome} ${tmpWarDir}/ext-2.2
+    checkExec "copying ExtJS files into staging"
+  fi
+
+  if [ "${addJars}" = "true" ]; then
+    if [ ! "${components}" = "" ];then
+      components="${components}, "
+    fi
+    components="${components}JARs"
+
+    for jarPath in ${jarsPath//:/$'\n'}
+    do
+      found=`ls ${tmpWarDir}/WEB-INF/lib/${jarPath} 2> /dev/null | wc -l`
+      checkExec "looking for JAR ${jarPath} in input WAR"
+      if [ ! $found = 0 ]; then
+        echo
+        echo "Specified Oozie WAR '${inputWar}' already contains JAR ${jarPath}"
+        echo
+        cleanUp
+        exit -1
+      fi
+      cp ${jarPath} ${tmpWarDir}/WEB-INF/lib/
+      checkExec "copying jar ${jarPath} to staging"
+    done
+  fi
+
+  #Creating new Oozie WAR
+  currentDir=`pwd`
+  cd ${tmpWarDir}
+  zip -r oozie.war * > /dev/null
+  checkExec "creating new Oozie WAR"
+  cd ${currentDir}
+
+  #copying new Oozie WAR to asked location
+  cp ${tmpWarDir}/oozie.war ${outputWar}
+  checkExec "copying new Oozie WAR"
+
+  echo
+  echo "New Oozie WAR file with added '${components}' at ${outputWar}"
+  echo
+  cleanUp
 
   if [ "$?" != "0" ]; then
     exit -1
