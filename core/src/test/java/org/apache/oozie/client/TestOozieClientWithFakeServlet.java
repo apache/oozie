fc00dcc2f39be1a993b8029c451fd5b0788d2de2
virag <virag@unknown>
2013-05-14 17:24:36 +0000

    OOZIE-1313 coverage fix for org.apache.oozie.client (aleksgor via virag)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1482475 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/oozie/client/TestOozieClientWithFakeServlet.java
@@ -0,0 +1,197 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oozie.client;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.oozie.BundleJobBean;
+import org.apache.oozie.CoordinatorActionBean;
+import org.apache.oozie.CoordinatorJobBean;
+import org.apache.oozie.WorkflowJobBean;
+import org.apache.oozie.client.CoordinatorAction.Status;
+import org.apache.oozie.client.rest.BulkResponseImpl;
+import org.apache.oozie.client.rest.JsonTags;
+import org.json.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+
+/**
+ * Test some client functions with fake http connect
+ *
+ */
+public class TestOozieClientWithFakeServlet {
+
+    private int answer = 0;
+    private boolean check = true;
+
+    /**
+     * Test method getJMSTopicName
+     */
+    @Test
+    public void testGetJMSTopicName() throws Exception {
+        answer = 0;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        String answer = client.getJMSTopicName("jobId");
+        assertEquals("topicName", answer);
+
+    }
+
+    /**
+     * Test method getJMSConnectionInfo
+     */
+    @Test
+    public void testGetJMSConnectionInfo() throws Exception {
+        answer = 1;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        JMSConnectionInfo answer = client.getJMSConnectionInfo();
+        assertNotNull(answer);
+
+    }
+
+    /**
+     * Test method getCoordActionInfo
+     */
+    @Test
+    public void testGetCoordActionInfo() throws Exception {
+        answer = 1;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        CoordinatorAction answer = client.getCoordActionInfo("actiomId");
+        assertNotNull(answer);
+    }
+
+    /**
+     * Test method getBundleJobsInfo
+     */
+    @Test
+    public void testGetBundleJobsInfo() throws Exception {
+        answer = 2;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        List<BundleJob> answer = client.getBundleJobsInfo("", 0, 10);
+        assertNotNull(answer);
+        assertEquals(1, answer.size());
+
+    }
+
+    /**
+     * Test method getBulkInfo
+     */
+    @Test
+    public void testGetBulkInfo() throws Exception {
+        answer = 3;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        List<BulkResponse> answer = client.getBulkInfo("", 0, 10);
+        assertNotNull(answer);
+        assertEquals(2, answer.size());
+        assertEquals(Status.READY, answer.get(0).getAction().getStatus());
+
+    }
+
+    /**
+     * Test method FakeOozieClient
+     */
+    @Test
+    public void testBundleRerun() throws Exception {
+        answer = 1;
+        check = true;
+        FakeOozieClient client = new FakeOozieClient("http://url");
+        Void answer = client.reRunBundle("jobId", "", "", true, true);
+        assertNull(answer);
+
+    }
+
+    private class FakeOozieClient extends OozieClient {
+
+        public FakeOozieClient(String oozieUrl) {
+            super(oozieUrl);
+        }
+
+        @Override
+        protected HttpURLConnection createConnection(URL url, String method) throws IOException, OozieClientException {
+            HttpURLConnection result = mock(HttpURLConnection.class);
+            when(result.getResponseCode()).thenReturn(200);
+            when(result.getInputStream()).thenReturn(getIs());
+            return result;
+        }
+
+        @SuppressWarnings("unchecked")
+        private InputStream getIs() {
+            ByteArrayInputStream result = new ByteArrayInputStream("".getBytes());
+            if (check) {
+                JSONArray array = new JSONArray();
+                array.put(2L);
+                String s = JSONValue.toJSONString(array);
+                result = new ByteArrayInputStream(s.getBytes());
+                check = false;
+                return result;
+            }
+            if (answer == 0) {
+                JSONObject json = new JSONObject();
+                json.put(JsonTags.JMS_TOPIC_NAME, "topicName");
+                result = new ByteArrayInputStream(json.toJSONString().getBytes());
+
+            }
+            if (answer == 1) {
+                JSONObject json = new JSONObject();
+                result = new ByteArrayInputStream(json.toJSONString().getBytes());
+
+            }
+            if (answer == 2) {
+                JSONObject json = new JSONObject();
+                List<WorkflowJobBean> jsonWorkflows = new ArrayList<WorkflowJobBean>();
+                jsonWorkflows.add(new WorkflowJobBean());
+                json.put(JsonTags.BUNDLE_JOBS, WorkflowJobBean.toJSONArray(jsonWorkflows, "GMT"));
+                result = new ByteArrayInputStream(json.toJSONString().getBytes());
+
+            }
+            if (answer == 3) {
+                JSONObject json = new JSONObject();
+                List<BulkResponseImpl> jsonWorkflows = new ArrayList<BulkResponseImpl>();
+                BulkResponseImpl bulk = new BulkResponseImpl();
+                bulk.setBundle(new BundleJobBean());
+                bulk.setCoordinator(new CoordinatorJobBean());
+                CoordinatorActionBean action = new CoordinatorActionBean();
+                action.setStatus(Status.READY);
+                bulk.setAction(action);
+                jsonWorkflows.add(bulk);
+                jsonWorkflows.add(bulk);
+                json.put(JsonTags.BULK_RESPONSES, BulkResponseImpl.toJSONArray(jsonWorkflows, "GMT"));
+                result = new ByteArrayInputStream(json.toJSONString().getBytes());
+
+            }
+
+            return result;
+        }
+
+    }
+}
--- core/src/test/java/org/apache/oozie/client/TestWorkflowClient.java
+++ core/src/test/java/org/apache/oozie/client/TestWorkflowClient.java
@@ -27,6 +27,7 @@ import org.apache.oozie.client.OozieClient.SYSTEM_MODE;
 import org.apache.oozie.client.rest.RestConstants;
 import org.apache.oozie.servlet.DagServletTestCase;
 import org.apache.oozie.servlet.MockDagEngineService;
+import org.apache.oozie.servlet.SLAServlet;
 import org.apache.oozie.servlet.V0JobServlet;
 import org.apache.oozie.servlet.V0JobsServlet;
 import org.apache.oozie.servlet.V1AdminServlet;
@@ -34,6 +35,14 @@ import org.apache.oozie.servlet.V1JobServlet;
 import org.apache.oozie.servlet.V1JobsServlet;
 import org.apache.oozie.servlet.V2AdminServlet;
 import org.apache.oozie.servlet.V2JobServlet;
+import org.json.simple.JSONArray;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+
+import static org.mockito.Mockito.*;
 
 public class TestWorkflowClient extends DagServletTestCase {
 
@@ -46,14 +55,17 @@ public class TestWorkflowClient extends DagServletTestCase {
         new V1JobServlet();
         new V2JobServlet();
         new V2AdminServlet();
+        new SLAServlet();
     }
 
     private static final boolean IS_SECURITY_ENABLED = false;
     static final String VERSION = "/v" + OozieClient.WS_PROTOCOL_VERSION;
-    static final String[] END_POINTS = {"/versions", VERSION + "/jobs", VERSION + "/job/*", VERSION + "/admin/*"};
-    static final Class[] SERVLET_CLASSES = { HeaderTestingVersionServlet.class, V0JobsServlet.class,
-            V0JobServlet.class, V1AdminServlet.class, V2AdminServlet.class, V1JobServlet.class, V2JobServlet.class,
-            V1JobsServlet.class };
+    static final String[] END_POINTS = {"/versions", VERSION + "/jobs", VERSION + "/job/*", VERSION + "/admin/*",
+            VERSION + "/sla/*" };
+    @SuppressWarnings("rawtypes")
+    static final Class[] SERVLET_CLASSES = {HeaderTestingVersionServlet.class, V0JobsServlet.class,
+            V0JobServlet.class, V1AdminServlet.class, SLAServlet.class, V2AdminServlet.class,  V1JobServlet.class,
+            V2JobServlet.class, V1JobsServlet.class};
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -71,6 +83,9 @@ public class TestWorkflowClient extends DagServletTestCase {
     //
     //    }
 
+    /**
+     * Test methods for headers manipulation
+     */
     public void testHeaders() throws Exception {
         runTest(END_POINTS, SERVLET_CLASSES, IS_SECURITY_ENABLED, new Callable<Void>() {
             public Void call() throws Exception {
@@ -78,9 +93,21 @@ public class TestWorkflowClient extends DagServletTestCase {
                 String oozieUrl = getContextURL();
                 OozieClient wc = new OozieClient(oozieUrl);
                 wc.setHeader("header", "test");
+                assertEquals("test", wc.getHeader("header"));
+                assertEquals("test", wc.getHeaders().get("header"));
+
+                boolean found = false;
+                for (Iterator<String> headers = wc.getHeaderNames(); headers.hasNext();) {
+                    if ("header".equals(headers.next())) {
+                        found = true;
+                    }
+                }
+                assertTrue("headers does not contain header!", found);
                 wc.validateWSVersion();
                 assertTrue(HeaderTestingVersionServlet.OOZIE_HEADERS.containsKey("header"));
                 assertTrue(HeaderTestingVersionServlet.OOZIE_HEADERS.containsValue("test"));
+                wc.removeHeader("header");
+                assertNull(wc.getHeader("header"));
                 return null;
             }
         });
@@ -94,6 +121,15 @@ public class TestWorkflowClient extends DagServletTestCase {
                 OozieClient wc = new OozieClient(oozieUrl);
                 assertEquals(oozieUrl, wc.getOozieUrl().substring(0, wc.getOozieUrl().length() - 1));
                 assertTrue(wc.getProtocolUrl().startsWith(wc.getOozieUrl() + "v"));
+
+                try {
+                    wc = new OozieClientForTest(oozieUrl);
+                    wc.getProtocolUrl();
+                    fail("wrong version should run throw exception");
+                }
+                catch (OozieClientException e) {
+                    assertEquals("UNSUPPORTED_VERSION : Supported version [2] or less, Unsupported versions[-11-10]", e.toString());
+                }
                 return null;
             }
         });
@@ -382,5 +418,90 @@ public class TestWorkflowClient extends DagServletTestCase {
                 return null;
             }
         });
+    }
+
+    /**
+     * Test client's methods getWorkflowActionInfo and getBundleJobInfo
+     */
+    public void testJobInformation() throws Exception {
+        runTest(END_POINTS, SERVLET_CLASSES, IS_SECURITY_ENABLED, new Callable<Void>() {
+            public Void call() throws Exception {
+                String oozieUrl = getContextURL();
+                OozieClient wc = new OozieClient(oozieUrl);
+                String jobId = MockDagEngineService.JOB_ID + "1" + MockDagEngineService.JOB_ID_END;
+                assertEquals(RestConstants.JOB_SHOW_LOG, wc.getJobLog(jobId));
+
+                WorkflowAction wfAction = wc.getWorkflowActionInfo(jobId);
+
+                assertEquals(jobId, wfAction.getId());
+                CoordinatorJob job = wc.getCoordJobInfo(jobId);
+
+                assertEquals("group", job.getAcl());
+                assertEquals("SUCCEEDED", job.getStatus().toString());
+                assertEquals("user", job.getUser());
+
+                BundleJob bundleJob = wc.getBundleJobInfo(jobId);
+                assertEquals("SUCCEEDED", bundleJob.getStatus().toString());
+                assertEquals("user", bundleJob.getUser());
+
+                return null;
+            }
+        });
+    }
+
+
+    /**
+     * Test SlaServlet and client's method getSlaInfo
+     */
+    public void testSla() throws Exception {
+        runTest(END_POINTS, SERVLET_CLASSES, IS_SECURITY_ENABLED, new Callable<Void>() {
+            public Void call() throws Exception {
+                String oozieUrl = getContextURL();
+                OozieClient wc = new OozieClient(oozieUrl);
+
+                PrintStream oldStream = System.out;
+                ByteArrayOutputStream data = new ByteArrayOutputStream();
+                System.setOut(new PrintStream(data));
+                try {
+                    wc.getSlaInfo(0, 10, null);
+                }
+                finally {
+                    System.setOut(oldStream);
+                }
+                assertTrue(data.toString().contains("<sla-message>"));
+                assertTrue(data.toString().contains("<last-sequence-id>0</last-sequence-id>"));
+                assertTrue(data.toString().contains("</sla-message>"));
+
+                return null;
+            }
+        });
+    }
+
+    /**
+     * Fake class for test reaction on a bad version
+     */
+    private class OozieClientForTest extends OozieClient {
+
+        public OozieClientForTest(String oozieUrl) {
+            super(oozieUrl);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected HttpURLConnection createConnection(URL url, String method) throws IOException, OozieClientException {
+            HttpURLConnection result = mock(HttpURLConnection.class);
+            when(result.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+
+            JSONArray versions = new JSONArray();
+            versions.add(-11);
+            versions.add(-10);
+            Writer writer = new StringWriter();
+            versions.writeJSONString(writer);
+            writer.flush();
+
+            when(result.getInputStream()).thenReturn(new ByteArrayInputStream(writer.toString().getBytes()));
+            return result;
+        }
+
     }
 }
