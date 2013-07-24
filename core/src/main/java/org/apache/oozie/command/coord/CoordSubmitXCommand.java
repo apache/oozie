9b03d4b810eb063c973d948b11b8c354f873d5df
rkanter <rkanter@unknown>
2013-07-24 00:49:10 +0000

    OOZIE-1425 param checker should validate cron syntax (bowenzhangusa via rkanter)
    git-svn-id: https://svn.apache.org/repos/asf/oozie/trunk@1506380 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/oozie/command/coord/CoordSubmitXCommand.java
+++ core/src/main/java/org/apache/oozie/command/coord/CoordSubmitXCommand.java
@@ -625,9 +625,10 @@ public class CoordSubmitXCommand extends SubmitTransitionXCommand {
         // job's main attributes
         // frequency
         String val = resolveAttribute("frequency", eAppXml, evalFreq);
-        int ival = ParamChecker.checkInteger(val, "frequency");
-        ParamChecker.checkGTZero(ival, "frequency");
-        coordJob.setFrequency(Integer.toString(ival));
+        int ival = 0;
+
+        val = ParamChecker.checkFrequency(val);
+        coordJob.setFrequency(val);
         TimeUnit tmp = (evalFreq.getVariable("timeunit") == null) ? TimeUnit.MINUTE : ((TimeUnit) evalFreq
                 .getVariable("timeunit"));
         addAnAttribute("freq_timeunit", eAppXml, tmp.toString());
