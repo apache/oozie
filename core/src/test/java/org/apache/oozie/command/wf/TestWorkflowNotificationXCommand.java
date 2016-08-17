568041dc878683f33e916fbf0f4ccb5811640b61
Robert Kanter <rkanter@cloudera.com>
2016-08-16 22:05:15 -0700

    OOZIE-2081 WorkflowJob notification to include coordinator action id (seoeun25 via rkanter)

--- core/src/test/java/org/apache/oozie/command/wf/TestWorkflowNotificationXCommand.java
+++ core/src/test/java/org/apache/oozie/command/wf/TestWorkflowNotificationXCommand.java
@@ -30,8 +30,38 @@ import org.apache.oozie.workflow.WorkflowInstance;
 import org.junit.Assert;
 import org.mockito.Mockito;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TestWorkflowNotificationXCommand extends XTestCase {
     private EmbeddedServletContainer container;
+
+    public static class CallbackServlet extends HttpServlet {
+        public static volatile String JOB_ID = null;
+        public static String NODE_NAME = null;
+        public static String STATUS = null;
+        public static String PARENT_ID = null;
+
+        public static void reset() {
+            JOB_ID = null;
+            NODE_NAME = null;
+            STATUS = null;
+            PARENT_ID = null;
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            JOB_ID = req.getParameter("jobId");
+            NODE_NAME = req.getParameter("nodeName");
+            STATUS = req.getParameter("status");
+            PARENT_ID = req.getParameter("parentId");
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
 
     @Override
     public void setUp() throws Exception {
@@ -41,6 +71,8 @@ public class TestWorkflowNotificationXCommand extends XTestCase {
         services.init();
         container = new EmbeddedServletContainer("blah");
         container.addServletEndpoint("/hang/*", HangServlet.class);
+        CallbackServlet.reset();
+        container.addServletEndpoint("/callback/*", CallbackServlet.class);
         container.start();
     }
 
@@ -75,5 +107,40 @@ public class TestWorkflowNotificationXCommand extends XTestCase {
         long end = System.currentTimeMillis();
         Assert.assertTrue(end - start >= 50);
         Assert.assertTrue(end - start < 10000);
+    }
+
+    public void testWFNotification() throws Exception {
+
+        String notificationUrl = "/callback/wf?jobId=$jobId&parentId=$parentId";
+        _testNotificationParentId(notificationUrl, "1", null, "");
+
+        notificationUrl = "/callback/wf?jobId=$jobId";
+        _testNotificationParentId(notificationUrl, "1", null, null);
+
+        notificationUrl = "/callback/wf?jobId=$jobId&parentId=$parentId";
+        _testNotificationParentId(notificationUrl, "1", "0000000-111111-oozie-XXX-C@1", "0000000-111111-oozie-XXX-C@1");
+
+        notificationUrl = "/callback/wf?jobId=$jobId";
+        _testNotificationParentId(notificationUrl, "1", "0000000-111111-oozie-XXX-C@1", null);
+
+    }
+
+    private void _testNotificationParentId(String notificationUrl, String jobId, String parentId, String expectedParentId)
+            throws Exception{
+        XConfiguration conf = new XConfiguration();
+        conf.set(OozieClient.WORKFLOW_NOTIFICATION_URL, container.getServletURL(notificationUrl));
+        WorkflowInstance wfi = Mockito.mock(WorkflowInstance.class);
+        Mockito.when(wfi.getConf()).thenReturn(conf);
+        WorkflowJobBean workflow = Mockito.mock(WorkflowJobBean.class);
+        Mockito.when(workflow.getId()).thenReturn(jobId);
+        Mockito.when(workflow.getStatus()).thenReturn(WorkflowJob.Status.SUCCEEDED);
+        Mockito.when(workflow.getParentId()).thenReturn(parentId);
+        Mockito.when(workflow.getWorkflowInstance()).thenReturn(wfi);
+        WorkflowNotificationXCommand command = new WorkflowNotificationXCommand(workflow);
+        command.setRetry(3);
+        command.call();
+
+        Assert.assertEquals(jobId, CallbackServlet.JOB_ID);
+        Assert.assertEquals(expectedParentId, CallbackServlet.PARENT_ID);
     }
 }
