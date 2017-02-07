3709ea2f5abb5c5b0de3fba064e3fe4e39157c6d
Robert Kanter <rkanter@cloudera.com>
2017-02-07 13:50:42 -0800

    OOZIE-2786 Pass Oozie workflow ID and settings to Spark application configuration (zhengxb2005 via rkanter)
    (cherry picked from commit a8c3392570ab46517c995660c922257059272f70)

--- sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkMain.java
+++ sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkMain.java
@@ -26,12 +26,16 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.oozie.util.IOUtils;
 import org.apache.oozie.util.XConfiguration;
+
+import com.google.common.collect.Lists;
 
 public class TestSparkMain extends MainTestCase {
 
@@ -119,5 +123,18 @@ public class TestSparkMain extends MainTestCase {
         for (String s : jarList) {
             assertFalse(pattern.matcher(s).find());
         }
+    }
+
+    public void testAppendOoziePropertiesToSparkConf() throws Exception {
+        SparkMain instance = SparkMain.class.newInstance();
+        List<String> sparkArgs = new ArrayList<String>();
+        Configuration actionConf = new Configuration();
+        actionConf.set("foo", "foo-not-to-include");
+        actionConf.set("oozie.launcher", "launcher-not-to-include");
+        actionConf.set("oozie.spark", "spark-not-to-include");
+        actionConf.set("oozie.bar", "bar");
+
+        instance.appendOoziePropertiesToSparkConf(sparkArgs, actionConf);
+        assertEquals(Lists.newArrayList("--conf", "spark.oozie.bar=bar"), sparkArgs);
     }
 }
