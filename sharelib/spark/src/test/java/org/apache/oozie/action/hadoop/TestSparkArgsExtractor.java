1ae68908b26e7a36089c47ca64c3c683dc2f6099
Gezapeti Cseh <gezapeti@gmail.com>
2017-07-28 13:33:40 +0200

    OOZIE-2984 Parse spark-defaults.conf values with spaces without needing the quotes (andras.piros via gezapeti)

--- sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkArgsExtractor.java
+++ sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkArgsExtractor.java
@@ -194,4 +194,41 @@ public class TestSparkArgsExtractor {
                         "arg1"),
                 sparkArgs);
     }
+
+    @Test
+    public void testQuotedDriverAndExecutorExtraJavaOptionsParsing() throws Exception {
+        final Configuration actionConf = new Configuration();
+        actionConf.set(SparkActionExecutor.SPARK_MASTER, "yarn");
+        actionConf.set(SparkActionExecutor.SPARK_MODE, "client");
+        actionConf.set(SparkActionExecutor.SPARK_CLASS, "org.apache.oozie.example.SparkFileCopy");
+        actionConf.set(SparkActionExecutor.SPARK_JOB_NAME, "Spark Copy File");
+        actionConf.set(SparkActionExecutor.SPARK_OPTS,
+                "--conf spark.executor.extraJavaOptions=\"-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp\"" +
+                "--conf spark.driver.extraJavaOptions=\"-Xmn2703m -XX:SurvivorRatio=2 -XX:ParallelGCThreads=20\"");
+        actionConf.set(SparkActionExecutor.SPARK_JAR, "/lib/test.jar");
+
+        final String[] mainArgs = {"arg0", "arg1"};
+        final List<String> sparkArgs = new SparkArgsExtractor(actionConf).extract(mainArgs);
+
+        assertEquals("Spark args mismatch",
+                Lists.newArrayList("--master", "yarn",
+                        "--deploy-mode", "client",
+                        "--name", "Spark Copy File",
+                        "--class", "org.apache.oozie.example.SparkFileCopy",
+                        "--conf", "spark.executor.extraJavaOptions=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp " +
+                                "-Dlog4j.configuration=spark-log4j.properties",
+                        "--conf", "spark.driver.extraJavaOptions=-Xmn2703m -XX:SurvivorRatio=2 -XX:ParallelGCThreads=20 " +
+                                "-Dlog4j.configuration=spark-log4j.properties",
+                        "--conf", "spark.executor.extraClassPath=$PWD/*",
+                        "--conf", "spark.driver.extraClassPath=$PWD/*",
+                        "--conf", "spark.yarn.security.tokens.hive.enabled=false",
+                        "--conf", "spark.yarn.security.tokens.hbase.enabled=false",
+                        "--files", "spark-log4j.properties,hive-site.xml",
+                        "--conf", "spark.yarn.jar=null",
+                        "--verbose",
+                        "/lib/test.jar",
+                        "arg0",
+                        "arg1"),
+                sparkArgs);
+    }
 }
--- sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkOptionsSplitter.java
+++ sharelib/spark/src/test/java/org/apache/oozie/action/hadoop/TestSparkOptionsSplitter.java
@@ -34,13 +34,35 @@ public class TestSparkOptionsSplitter {
     @Parameterized.Parameters
     public static List<Object[]> params() {
         return Arrays.asList(new Object[][]{
-                {"--option1 value1", Arrays.asList("--option1", "value1")},
-                {"--option1   value1", Arrays.asList("--option1", "value1")},
-                {"   --option1 value1   ", Arrays.asList("--option1", "value1")},
-                {"--conf special=value1", Arrays.asList("--conf", "special=value1")},
-                {"--conf special=\"value1\"", Arrays.asList("--conf", "special=value1")},
-                {"--conf special=\"value1 value2\"", Arrays.asList("--conf", "special=value1 value2")},
-                {" --conf special=\"value1 value2\"  ", Arrays.asList("--conf", "special=value1 value2")},
+                {"--option1 value1",
+                        Arrays.asList("--option1", "value1")},
+                {"--option1   value1",
+                        Arrays.asList("--option1", "value1")},
+                {"   --option1 value1   ",
+                        Arrays.asList("--option1", "value1")},
+                {"--conf special=value1",
+                        Arrays.asList("--conf", "special=value1")},
+                {"--conf special=\"value1\"",
+                        Arrays.asList("--conf", "special=value1")},
+                {"--conf special=\"value1 value2\"",
+                        Arrays.asList("--conf", "special=value1 value2")},
+                {" --conf special=\"value1 value2\"  ",
+                        Arrays.asList("--conf", "special=value1 value2")},
+                {"--conf special=value1 value2 --conf value3",
+                        Arrays.asList("--conf", "special=value1 value2", "--conf", "value3")},
+                {"--conf special1=value1 special2=\"value2 value3\" --conf value4",
+                        Arrays.asList("--conf", "special1=value1 special2=\"value2 value3\"", "--conf", "value4")},
+                {"--conf special1=value1 value2 special2=value3 value4",
+                        Arrays.asList("--conf", "special1=value1 value2 special2=value3 value4")},
+                {"--option1 value1 --option2=value2",
+                        Arrays.asList("--option1", "value1", "--option2", "value2")},
+                {"--conf spark.executor.extraJavaOptions=\"-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp\"",
+                    Arrays.asList("--conf",
+                            "spark.executor.extraJavaOptions=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp")},
+                {"--option1 value1 --verbose",
+                        Arrays.asList("--option1", "value1", "--verbose")},
+                {"--verbose --option1 value1",
+                        Arrays.asList("--verbose", "--option1", "value1")}
         });
     }
 
