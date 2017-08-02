311fda18183091138ce3c4ce9fbf878d478a3861
Attila Sasvari <asasvari@cloudera.com>
2017-08-02 12:32:43 +0200

    OOZIE-3021 Error on job or SLA event listening: WARN logs instead of DEBUG (andras.piros via asasvari)

--- core/src/main/java/org/apache/oozie/service/EventHandlerService.java
+++ core/src/main/java/org/apache/oozie/service/EventHandlerService.java
@@ -30,6 +30,7 @@ import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.oozie.ErrorCode;
+import org.apache.oozie.action.hadoop.PasswordMasker;
 import org.apache.oozie.client.event.Event;
 import org.apache.oozie.client.event.Event.MessageType;
 import org.apache.oozie.client.event.JobEvent;
@@ -254,6 +255,9 @@ public class EventHandlerService implements Service {
                                 catch (Throwable error) {
                                     XLog.getLog(EventHandlerService.class).debug("Throwable in EventWorker thread run : ",
                                             error);
+                                    XLog.getLog(EventHandlerService.class).warn("Throwable in EventWorker thread run. " +
+                                                    "Error message: {0}",
+                                            new PasswordMasker().maskPasswordsIfNecessary(error.getMessage()));
                                 }
                             }
                         }
@@ -263,6 +267,9 @@ public class EventHandlerService implements Service {
             catch (Throwable error) {
                 XLog.getLog(EventHandlerService.class).debug("Throwable in EventWorker thread run : ",
                         error);
+                XLog.getLog(EventHandlerService.class).warn("Throwable in EventWorker thread run. " +
+                                "Error message: {0}",
+                        new PasswordMasker().maskPasswordsIfNecessary(error.getMessage()));
             }
         }
 
