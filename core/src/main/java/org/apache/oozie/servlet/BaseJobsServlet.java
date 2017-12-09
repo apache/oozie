417894d6abe196e3ab172fb4fb5708d938f63c71
satishsaley <satishsaley@apache.org>
2017-12-08 17:22:15 -0800

    OOZIE-3127 Remove redundant check for user (satishsaley)

--- core/src/main/java/org/apache/oozie/servlet/BaseJobsServlet.java
+++ core/src/main/java/org/apache/oozie/servlet/BaseJobsServlet.java
@@ -92,7 +92,6 @@ public abstract class BaseJobsServlet extends JsonRestServlet {
         conf = conf.trim();
         conf = conf.resolve();
 
-        validateJobConfiguration(conf);
         String requestUser = getUser(request);
         if (!requestUser.equals(UNDEF)) {
             conf.set(OozieClient.USER_NAME, requestUser);
@@ -250,10 +249,4 @@ public abstract class BaseJobsServlet extends JsonRestServlet {
     abstract JSONObject getJobs(HttpServletRequest request)
     throws XServletException, IOException;
 
-    static void validateJobConfiguration(Configuration conf) throws XServletException {
-        if (conf.get(OozieClient.USER_NAME) == null) {
-            throw new XServletException(HttpServletResponse.SC_BAD_REQUEST, ErrorCode.E0401,
-                    OozieClient.USER_NAME);
-        }
-    }
 }
