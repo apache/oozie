3d1fe2877b654f0a9a80df16ce7ed04314426e2a
Andras Piros <andras.piros@cloudera.com>
2018-08-06 18:17:55 +0200

    OOZIE-3315 DateList example fails (daniel.becker via andras.piros)

--- examples/src/main/java/org/apache/oozie/example/DateList.java
+++ examples/src/main/java/org/apache/oozie/example/DateList.java
@@ -20,11 +20,14 @@ package org.apache.oozie.example;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 import java.util.TimeZone;
 
@@ -32,41 +35,74 @@ public class DateList {
     private static final TimeZone UTC = getTimeZone("UTC");
     private static String DATE_LIST_SEPARATOR = ",";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, ParseException {
+        if (!checkArgsOk(args)) {
+            System.exit(1);
+        }
+
+        String dateList = createDateListFromArgs(args);
+
+        System.out.println("datelist :" + dateList+ ":");
+        writeWorkflowOutput(dateList);
+    }
+
+    private static boolean checkArgsOk(final String[] args) {
         if (args.length < 5) {
             System.out
                     .println("Usage: java DateList <start_time>  <end_time> <frequency> <timeunit> <timezone>");
             System.out
                     .println("Example: java DateList 2009-02-01T01:00Z 2009-02-01T02:00Z 15 MINUTES UTC");
-            System.exit(1);
-        }
-        Date startTime = parseDateUTC(args[0]);
-        Date endTime = parseDateUTC(args[1]);
-        Repeatable rep = new Repeatable();
-        rep.setBaseline(startTime);
-        rep.setFrequency(Integer.parseInt(args[2]));
-        rep.setTimeUnit(TimeUnit.valueOf(args[3]));
-        rep.setTimeZone(getTimeZone(args[4]));
-        int occurrence = 0;
-        Date date = rep.getOccurrenceTime(startTime, occurrence++, null);
-        StringBuilder dateList = new StringBuilder();
-        while (date != null && date.before(endTime)) {
-            date = rep.getOccurrenceTime(startTime, occurrence++, null);
-            if (occurrence > 1) {
-                dateList.append(DATE_LIST_SEPARATOR);
-            }
-            dateList.append(formatDateUTC(date));
+            return false;
         }
 
-        System.out.println("datelist :" + dateList+ ":");
+        return true;
+    }
+
+    private static String createDateListFromArgs(final String[] args) throws ParseException {
+        final Date startTime = parseDateUTC(args[0]);
+        final Date endTime = parseDateUTC(args[1]);
+        final int frequency = Integer.parseInt(args[2]);
+        final TimeUnit timeUnit = TimeUnit.valueOf(args[3]);
+        final TimeZone timeZone = getTimeZone(args[4]);
+        final Repeatable rep = createRepeatable(startTime, frequency, timeUnit, timeZone);
+
+        return getDateList(startTime, endTime, rep);
+    }
+
+    private static Repeatable createRepeatable(final Date startTime, final int frequency,
+                                               final TimeUnit timeUnit, final TimeZone timeZone) {
+        Repeatable rep = new Repeatable();
+        rep.setBaseline(startTime);
+        rep.setFrequency(frequency);
+        rep.setTimeUnit(timeUnit);
+        rep.setTimeZone(timeZone);
+
+        return rep;
+    }
+
+    private static String getDateList(final Date startTime, final Date endTime, final Repeatable rep) {
+        int occurrence = 0;
+        List<String> dates = new ArrayList<>();
+        Date date = rep.getOccurrenceTime(startTime, occurrence++, null);
+
+        while (date != null && date.before(endTime)) {
+            dates.add(formatDateUTC(date));
+            date = rep.getOccurrenceTime(startTime, occurrence++, null);
+        }
+
+        return String.join(DATE_LIST_SEPARATOR, dates);
+    }
+
+    private static void writeWorkflowOutput(final String dateList) throws IOException {
         //Passing the variable to WF that could be referred by subsequent actions
         File file = new File(System.getProperty("oozie.action.output.properties"));
         Properties props = new Properties();
-        props.setProperty("datelist", dateList.toString());
+        props.setProperty("datelist", dateList);
         try (OutputStream os = new FileOutputStream(file)) {
             props.store(os, "");
         }
     }
+
     //Utility methods
     private static DateFormat getISO8601DateFormat() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
@@ -82,11 +118,11 @@ public class DateList {
         return tz;
     }
 
-    private static Date parseDateUTC(String s) throws Exception {
+    private static Date parseDateUTC(String s) throws ParseException {
         return getISO8601DateFormat().parse(s);
     }
 
-    private static String formatDateUTC(Date d) throws Exception {
+    private static String formatDateUTC(Date d) {
         return (d != null) ? getISO8601DateFormat().format(d) : "NULL";
     }
 }
